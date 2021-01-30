dbeb1eec177cc1726948e1a07257296d8bc5f3c6
Douglas Byfield <dougyd30@gmail.com>
2021-01-29 23:11:38 -0500

    completing exercise and readme

--- hello.py
+++ hello.py
@@ -1,8 +1,37 @@
 '''
-    Author: Douglas Byfield
+ Douglas Byfield
     
 '''
 
 def hello():
     print("ECSE3038 - Engineering IoT Systems")
 
+hello()
+
+
+def validatePassword(psword):
+    alnum, num = 0, 0
+
+    if len(psword) >= 8 and psword.isalnum():
+        for val in psword:
+            try:
+                if isinstance(int(val), int):
+                    num += 1
+            except:
+                pass
+        
+        if num >= 2:
+            return True
+    
+    return False
+
+def sumUpToN(num):
+    digit = 0
+
+    if num > 1:
+        for i in range(1, num+1):
+            digit += i
+    else:
+        digit = -1
+
+    return digit
